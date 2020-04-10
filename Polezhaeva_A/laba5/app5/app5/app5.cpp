7d065db06404acacfeae769ca584bf1eb32b2a79
FuramyD <42672553+FuramyD@users.noreply.github.com>
2020-04-10 12:51:24 +0300

    Add files via upload

---
+++ Polezhaeva_A/laba5/app5/app5/app5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int size;
+	std::cout << "Enter size: "; std::cin >> size;
+	int* a = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < size; i++)
+	{
+		int temp = a[i];
+		int j = i - 1;
+		while (j >= 0 && a[j] > temp)
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = temp;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << a[i] << ' ';
+	}
+	return 0;
+}
